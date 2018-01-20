ba294cd120afc24276d9925febdc72488df812f5
Tom Eliason <tom.eliason@yahoo.com>
2018-01-20 16:00:29 -0700

    testinstance

---
+++ testinstance/variables.tf
@@ -0,0 +1,30 @@
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {}
+
+variable "compartment_ocid" {}
+variable "ssh_public_key" {}
+variable "ssh_private_key" {}
+
+# Choose an Availability Domain
+variable "AD" {
+    default = "1"
+}
+
+variable "InstanceShape" {
+    default = "VM.Standard1.1"
+}
+
+variable "InstanceImageDisplayName" {
+    default = "Oracle-Linux-7.4-2018.01.10-0"
+}
+
+variable "DBSize" {
+    default = "50" // size in GBs
+}
+
+variable "BootStrapFile" {
+    default = "./userdata/bootstrap"
+}
